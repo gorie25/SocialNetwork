168afafc6b5564e124fe68597e2b49b1bed0947e
Goriee <21520481@gm.edu.vn>
2026-03-06 00:34:43 +0700

    Initial Commit

---
+++ SocialNetwork/SocialNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialNetworkApp.swift
+//  SocialNetwork
+//
+//  Created by Tien on 6/3/26.
+//
+
+import SwiftUI
+
+@main
+struct SocialNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
